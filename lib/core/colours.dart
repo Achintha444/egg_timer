617ec7458ce6c7806f2e8b3ed37008456d4f9115
Achinths Isuru <achinthaisuru444@gmail.com>
2020-07-03 22:31:12 +0530

    splash screen finished

--- lib/core/colours.dart
+++ lib/core/colours.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
 
 const Color ACCENT_COLOUR = Color(0xffF2F2F2);
-const Color MAIN_COLOUR = Color(0xffF4F4F4); 
+const Color MAIN_COLOUR = Color(0xffF4F4F4);
+const Color THIRD_COLOUR = Color(0xff3a3a3a);
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,6 +1,30 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+import 'core/colours.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = new Duration(seconds: 3);
+    return new Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => Container()));
+  }
+
   @override
   Widget build(BuildContext context) {
     double _height = MediaQuery.of(context).size.height;
@@ -9,13 +33,31 @@ class SplashScreen extends StatelessWidget {
     return Container(
       height: _height,
       width: _width,
-      color: Theme.of(context).scaffoldBackgroundColor,
+      color: THIRD_COLOUR,
       child: new Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Image(
-              image: AssetImage('logo.png'),
+              image: AssetImage('assets/logo.png'),
+              height: _height / 3.7,
             ),
+            Padding(
+              padding: EdgeInsets.only(bottom: _height / 15),
+            ),
+            Text(
+              'EGG TIMER'.toUpperCase(),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Theme.of(context).primaryColor,
+                decoration: TextDecoration.none,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: _height / 22),
+            ),
+            CircularProgressIndicator(),
           ],
         ),
       ),
